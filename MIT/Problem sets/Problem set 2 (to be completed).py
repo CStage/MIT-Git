0cad69084ba1b4346430d14ab1a8bff04f20f7ad
CStage <c.stage@live.dk>
2019-08-21 23:00:32 +0200

    Cleaning Up

--- MIT/Problem sets/Problem set 2 (to be completed).py
+++
@@ -1,9 +0,0 @@
-x=1
-y=150
-PNA=(55,56,57,58,59,60,61,62)
-for a in range(0,y):
-    for b in range(0,y):
-        for c in range(0,y):
-            n=6*a+9*b+20*c
-            if n in range(0,101):
-                print("to get ", n, "nuggets. You need: ", a, "6-piece", b, "9-piece and", c, "20-piece")
